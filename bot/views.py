0b8c8823af7433f105f4bf2ecbd702a6982ffbe1
Imdeepmehta <itsdeepmehta25@gmail.com>
2020-09-28 19:53:05 +0530

    bug

--- bot/views.py
+++ bot/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from .video_yt_scrapper import search_yt, get_youtube_video_duration
+import json
 
+@csrf_exempt
 def yt_video_search_play(request, *args, **kwargs):
     if request.method == "POST":
-        print("[POST] ")
-        query = request.POST['videoName']
+        print("[POST]")
+        client_data = json.loads(request.body.decode('utf-8'))
+        query = client_data['videoQuery']
         print(query)
         searchResult = search_yt(query)
         # print("searchResult", searchResult)
         context = {
             "videoId" : searchResult['items'][0]['id']['videoId'],
         }
-        return render(request, 'index.html',context=context)
+        # context = json.dumps(context)
+        print(context)
+        return JsonResponse(context)
     else:
         return render(request, 'index.html',)
